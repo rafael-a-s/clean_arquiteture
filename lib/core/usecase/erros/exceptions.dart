da3625828cb758842488268a795fb96b0b5ce552
rafael-a-s <rafinha.rafaelaguiar@gmail.com>
2023-03-02 21:53:36 -0300

    Implementa Core

---
+++ lib/core/usecase/erros/exceptions.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+
+class ServerException extends Equatable implements Exception {
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+
+}
---
+++ lib/core/usecase/usecase.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:my_app/core/usecase/erros/failures.dart';
+
+
+abstract class Usecase<Output, Input> {
+  Future<Either<Failure, Output>> call(Input params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object> get props => [];
+}
