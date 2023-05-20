39900f1d4ac14f0b78fbe3f8d8ade103ce367fe2
Rafael Aguiar <rafinha.rafaelaguiar@gmail.com>
2023-05-20 10:40:45 -0300

    feat: Imp get all portifolios

--- lib/features/presenter/controllers/portifolio/geral_page_store.dart
+++ lib/features/presenter/controllers/portifolio/geral_page_store.dart
@@ -3,14 +3,19 @@ import 'dart:ffi';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'package:my_app/core/usecase/erros/failures.dart';
 import 'package:my_app/core/usecase/usecase.dart';
+import 'package:my_app/features/domain/entities/assets.dart';
+import 'package:my_app/features/domain/entities/portifolio.dart';
 import 'package:my_app/features/domain/entities/portifolio/portifolio_info.dart';
+import 'package:my_app/features/domain/usecases/portifolio/get_all_portifolios.dart';
 import 'package:my_app/features/domain/usecases/portifolio/get_infos_portifolio.dart';
 
 class GeralPageStore extends NotifierStore<Failure, PortifolioInfo> {
   final GetInfoPortifolioUseCase getInfoUsecase;
+  final GetAllPortifolioUseCase getAllPortifolioUseCase;
 
   GeralPageStore(
     this.getInfoUsecase,
+    this.getAllPortifolioUseCase,
   ) : super(
           const PortifolioInfo(
             total: 0,
@@ -19,6 +24,25 @@ class GeralPageStore extends NotifierStore<Failure, PortifolioInfo> {
             percent: 0,
           ),
         );
+  Future<List<Portifolio>> getAllPortifolio() async {
+    setLoading(true);
+    final result = await getAllPortifolioUseCase(NoParams());
+    setLoading(false);
+    return result.fold(
+      (error) => <Portifolio>[
+        const Portifolio(
+          id: '',
+          name: '',
+          coin: '',
+          subTotal: 0,
+          totalPriceActual: 0,
+          percent: 0,
+          assets: [Assets(symbol: '', quanty: 0, price: 0)],
+        ),
+      ],
+      (sucess) => sucess,
+    );
+  }
 
   Future<PortifolioInfo> getInfoAboutPortifolio() async {
     setLoading(true);
