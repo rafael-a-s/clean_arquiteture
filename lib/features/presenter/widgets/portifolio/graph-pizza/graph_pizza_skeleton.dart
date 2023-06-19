b633e9c5d17d794b7e3eeffd632c0cf29ee21a7b
Rafael Aguiar <rafinha.rafaelaguiar@gmail.com>
2023-06-19 18:58:56 -0300

    feat: Skeleton graph pizza of to loading

---
+++ lib/features/presenter/widgets/portifolio/graph-pizza/graph_pizza_skeleton.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/features/presenter/widgets/base/skeleton/skeleton.dart';
+
+class GraphPortifolioSkeleton extends StatefulWidget {
+  GraphPortifolioSkeleton({
+    super.key,
+  });
+
+  @override
+  State<GraphPortifolioSkeleton> createState() =>
+      _GraphPortifolioSkeletonState();
+}
+
+class _GraphPortifolioSkeletonState extends State<GraphPortifolioSkeleton> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Skeleton(
+          height: MediaQuery.of(context).size.height / 2.5,
+          width: MediaQuery.of(context).size.width,
+          isCircle: true,
+        ),
+        Column(
+          children: const [
+            Skeleton(
+              height: 25,
+              width: 75,
+            ),
+            Skeleton(
+              height: 25,
+              width: 40,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
