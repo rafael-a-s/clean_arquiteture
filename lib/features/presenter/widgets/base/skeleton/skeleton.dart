60394181a42b8fb7e8495bb0d6dd38776de2b7b1
Rafael Aguiar <rafinha.rafaelaguiar@gmail.com>
2023-06-19 18:59:22 -0300

    fix: add atribute circle, of to relatives contexts

--- lib/features/presenter/widgets/base/skeleton/skeleton.dart
+++ lib/features/presenter/widgets/base/skeleton/skeleton.dart
@@ -2,20 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:my_app/features/presenter/root.dart';
 
 class Skeleton extends StatelessWidget {
-  const Skeleton({Key? key, this.height, this.width}) : super(key: key);
+  const Skeleton({Key? key, this.height, this.width, this.isCircle})
+      : super(key: key);
 
   final double? height, width;
+  final bool? isCircle;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: height,
-      width: width,
-      padding: const EdgeInsets.all(RootStyle.defaultPadding / 2),
-      decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.04),
-          borderRadius: const BorderRadius.all(
-              Radius.circular(RootStyle.defaultPadding))),
-    );
+        height: height,
+        width: width,
+        padding: const EdgeInsets.all(RootStyle.defaultPadding / 2),
+        decoration:
+            isCircle != null ? circleInContainer() : noCircleInContainer());
   }
+
+  BoxDecoration circleInContainer() => BoxDecoration(
+      color: Colors.white.withOpacity(0.04), shape: BoxShape.circle);
+
+  BoxDecoration noCircleInContainer() => BoxDecoration(
+        color: Colors.white.withOpacity(0.04),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(RootStyle.defaultPadding),
+        ),
+      );
 }
