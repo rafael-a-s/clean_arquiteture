da6559d04713b889a888962762865bd6d37c1c1b
Rafael Aguiar <rafinha.rafaelaguiar@gmail.com>
2023-06-21 03:30:29 -0300

    feat: implement class when argumens for screen Sts

---
+++ lib/core/utils/status-screen-arguments/StatusScreenArgument.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StatusScreenArguments {
+  final bool isError;
+  final String message;
+  final VoidCallback onPressed;
+
+  StatusScreenArguments(
+      {required this.isError, required this.message, required this.onPressed});
+}
