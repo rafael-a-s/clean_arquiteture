761708fc4babdb7c27ae40b628110d9255384337
Rafael Aguiar <rafinha.rafaelaguiar@gmail.com>
2023-07-06 15:20:14 -0300

    feat: impl base reposiory

---
+++ lib/core/data/repositories/base_repository.dart
@@ -0,0 +1,68 @@
+import 'dart:ffi';
+
+import 'package:my_app/core/data/datasources/i_base_datasource.dart';
+import 'package:my_app/core/data/repositories/i_base_repository.dart';
+import 'package:my_app/core/domain/base_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:my_app/core/domain/base_model_convert.dart';
+import 'package:my_app/core/usecase/erros/exceptions.dart';
+import 'package:my_app/core/usecase/erros/failures.dart';
+
+abstract class BaseRepository<T extends BaseEntity, M extends BaseEntity, ID>
+    extends IBaseRepository<T, M, ID> {
+  late final IBaseDatasource datasource;
+
+  @override
+  Future<Either<Failure, List<T>>> getAll() async {
+    try {
+      final result = await datasource.getAll();
+      final ModelConvert<T, M> convert = getModelConvert();
+      return Right(
+          result.map((value) => convert.fromEntity(value as M)).toList());
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, T>> get() async {
+    try {
+      final result = await datasource.get(ID);
+      final ModelConvert<T, M> convert = getModelConvert();
+      return Right(convert.fromEntity(result as M));
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, T>> create(T t) async {
+    try {
+      final ModelConvert<T, M> convert = getModelConvert();
+      final result = await datasource.create(convert.toModel(t));
+      return Right(convert.fromEntity(result as M));
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, T>> update(T t) async {
+    try {
+      final ModelConvert<T, M> convert = getModelConvert();
+      final result = await datasource.update(convert.toModel(t));
+      return Right(convert.fromEntity(result as M));
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<void> delete(ID id) async {
+    try {
+      await datasource.delete(id);
+    } on ServerException {
+      Left(ServerFailure());
+    }
+  }
+}
---
+++ lib/core/data/repositories/i_base_repository.dart
@@ -0,0 +1,15 @@
+import 'dart:ffi';
+
+import 'package:my_app/core/domain/base_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:my_app/core/domain/base_model_convert.dart';
+import 'package:my_app/core/usecase/erros/failures.dart';
+
+abstract class IBaseRepository<T extends BaseEntity, M extends BaseEntity, ID> {
+  Future<Either<Failure, List<T>>> getAll();
+  Future<Either<Failure, T>> get();
+  Future<Either<Failure, T>> create(T t);
+  Future<Either<Failure, T>> update(T t);
+  Future<void> delete(ID id);
+  ModelConvert<T, M> getModelConvert();
+}
