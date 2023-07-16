45c293533c3d1628fe7ac00a1c9219236b9fd9ba
Rafael Aguiar <rafaelaguiar@unitins.br]>
2023-07-16 19:30:35 -0300

    feat: Nova forma de usar o bts implementadas

--- lib/features/presenter/modules/portifolio/pages/portifolio_page.dart
+++ lib/features/presenter/modules/portifolio/pages/portifolio_page.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:my_app/features/domain/entities/assets.dart';
 import 'package:my_app/features/domain/entities/portifolio.dart';
-import 'package:my_app/features/presenter/modules/portifolio/controller/portifolio_controller.dart';
+import 'package:my_app/features/presenter/modules/portifolio/widget/bottom-sheats/portifolio-page/modal_delete_portifolio.dart';
+import 'package:my_app/features/presenter/modules/portifolio/widget/bottom-sheats/portifolio-page/modal_edit_asset.dart';
+import 'package:my_app/features/presenter/modules/portifolio/widget/bottom-sheats/portifolio-page/modal_edit_name_portifolio.dart';
+import 'package:my_app/features/presenter/modules/portifolio/widget/bottom-sheats/portifolio-page/modal_trade_in_asset.dart';
 import 'package:my_app/features/presenter/modules/portifolio/widget/card-portifolio/card_portifolio.dart';
 import 'package:my_app/features/presenter/modules/portifolio/widget/list-portifolio/list_card.dart';
 import 'package:my_app/features/presenter/root.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:my_app/features/presenter/widgets/form/form_base.dart';
-import 'package:my_app/features/presenter/widgets/text_buttom_base/text_buttom_base.dart';
 
 class PortifolioPage extends StatefulWidget {
   Portifolio portifolio;
@@ -24,168 +24,40 @@ class PortifolioPage extends StatefulWidget {
 }
 
 class _PortifolioPage extends State<PortifolioPage> {
-  final controller = Modular.get<PortifolioController>();
-  bool _isLoading = false;
-  double total = 0.0;
-
-  final _formKey = GlobalKey<FormState>();
-  final _name = TextEditingController();
-  final _price = TextEditingController();
-  final _priceOfTradeIn = TextEditingController();
-  final _quanty = TextEditingController();
-
-  final String INVALID_LABEL = 'Campos obrigatórios não podem ser vazios!';
-
   void _modalEditNamePortifolio(context) {
-    _name.text = widget.portifolio.name;
     showModalBottomSheet(
         context: context,
         builder: (BuildContext bc) {
-          return FormBase(
-            title: 'Altere o nome do Portifolio',
-            buttom: TextButtomBase(
-              label: 'Alterar Portifolio',
-              onPressed: () {},
-            ),
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _name,
-                  keyboardType: TextInputType.text,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return INVALID_LABEL;
-                    }
-                    return null;
-                  },
-                  // ignore: prefer_const_constructors
-                  decoration: InputDecoration(
-                    label: const Text('Nome'),
-                    suffix: const Text('abc'),
-                  ),
-                ),
-              ],
-            ),
-          );
+          return ModalEditNamePortifolio(portifolio: widget.portifolio);
         });
   }
 
   void _modalEditPriceAsset(context, Assets asset) {
-    _price.text = asset.price.toString();
     showModalBottomSheet(
         context: context,
         builder: (BuildContext bc) {
-          return FormBase(
-            title: 'Altere o preço da Ordem',
-            buttom: TextButtomBase(
-              label: 'Enviar alteração',
-              onPressed: () {},
-            ),
-            child: Column(
-              children: [
-                TextFormField(
-                  autofocus: true,
-                  keyboardType: TextInputType.number,
-                  controller: _price,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  autovalidateMode: AutovalidateMode.always,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return INVALID_LABEL;
-                    }
-                    return null;
-                  },
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 40,
-                  ),
-                  // ignore: prefer_const_constructors
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    suffix: Text(
-                      widget.portifolio.coin,
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  onChanged: (value) => {
-                    setState(() {}),
-                  },
-                ),
-              ],
-            ),
+          return ModalEditAssetPortifolio(
+            id: widget.portifolio.id,
+            asset: asset,
           );
         });
   }
 
   void _modalTradeInAsset(context, Assets asset) {
-    _priceOfTradeIn.text = asset.price.toString();
-    _quanty.text = asset.quanty.toString();
     showModalBottomSheet(
         context: context,
         builder: (BuildContext bc) {
-          return FormBase(
-            title: 'Retirada de lucro',
-            buttom: TextButtomBase(
-              label: 'Confirmar venda',
-              onPressed: () {},
-            ),
-            child: Column(
-              children: [
-                TextFormField(
-                  autofocus: true,
-                  keyboardType: TextInputType.number,
-                  controller: _quanty,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  autovalidateMode: AutovalidateMode.always,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return INVALID_LABEL;
-                    }
-                    return null;
-                  },
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 30,
-                  ),
-                  // ignore: prefer_const_constructors
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    suffix: Text(
-                      widget.portifolio.coin,
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  onChanged: (value) => {
-                    setState(() {}),
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  controller: _priceOfTradeIn,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  autovalidateMode: AutovalidateMode.always,
-                  onChanged: (value) => {
-                    setState(() {}),
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return INVALID_LABEL;
-                    }
-                    return null;
-                  },
-                  // ignore: prefer_const_constructors
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    label: const Text('Preço por moeda'),
-                    hintText: 'Preço',
-                    suffix: const Text('\$'),
-                  ),
-                ),
-              ],
-            ),
+          return ModalTradeInAssetPortifolio(
+              id: widget.portifolio.id, asset: asset);
+        });
+  }
+
+  _modalDeletePortifolio(BuildContext context, dynamic id) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return ModalDeletePortifolio(
+            portifolio: widget.portifolio,
           );
         });
   }
@@ -249,7 +121,9 @@ class _PortifolioPage extends State<PortifolioPage> {
                   foregroundColor: Colors.white,
                   icon: Icons.remove,
                   label: 'Remover',
-                  onPressed: (BuildContext context) {},
+                  onPressed: (BuildContext context) {
+                    _modalDeletePortifolio(context, widget.portifolio.id);
+                  },
                 )
               ],
             ),
@@ -289,59 +163,53 @@ class _PortifolioPage extends State<PortifolioPage> {
               )
             ],
           ),
-          _isLoading
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Expanded(
-                  child: widget.portifolio.assets.isNotEmpty
-                      ? ListView.builder(
-                          itemCount: widget.portifolio.assets.length,
-                          itemBuilder: (context, item) {
-                            return Slidable(
-                              key: const ValueKey(0),
-                              startActionPane: ActionPane(
-                                motion: const ScrollMotion(),
-                                children: [
-                                  SlidableAction(
-                                    backgroundColor:
-                                        const Color(RootStyle.bgColor),
-                                    foregroundColor: Colors.white,
-                                    icon: Icons.edit,
-                                    label: 'Editar',
-                                    onPressed: (BuildContext context) {
-                                      _modalEditPriceAsset(context,
-                                          widget.portifolio.assets[item]);
-                                    },
-                                  ),
-                                ],
-                              ),
-                              endActionPane: ActionPane(
-                                motion: const ScrollMotion(),
-                                children: [
-                                  SlidableAction(
-                                    backgroundColor:
-                                        const Color(RootStyle.bgColor),
-                                    foregroundColor: Colors.red,
-                                    icon: Icons.swipe_down,
-                                    label: 'Vender',
-                                    onPressed: (BuildContext context) {
-                                      _modalTradeInAsset(context,
-                                          widget.portifolio.assets[item]);
-                                    },
-                                  )
-                                ],
-                              ),
-                              child: ListCardPortifolio(
-                                assets: widget.portifolio.assets[item],
-                              ),
-                            );
-                          },
-                        )
-                      : const Center(
-                          child: Text('Sem resultados encontrados.'),
+          Expanded(
+            child: widget.portifolio.assets.isNotEmpty
+                ? ListView.builder(
+                    itemCount: widget.portifolio.assets.length,
+                    itemBuilder: (context, item) {
+                      return Slidable(
+                        key: const ValueKey(0),
+                        startActionPane: ActionPane(
+                          motion: const ScrollMotion(),
+                          children: [
+                            SlidableAction(
+                              backgroundColor: const Color(RootStyle.bgColor),
+                              foregroundColor: Colors.white,
+                              icon: Icons.edit,
+                              label: 'Editar',
+                              onPressed: (BuildContext context) {
+                                _modalEditPriceAsset(
+                                    context, widget.portifolio.assets[item]);
+                              },
+                            ),
+                          ],
                         ),
-                ),
+                        endActionPane: ActionPane(
+                          motion: const ScrollMotion(),
+                          children: [
+                            SlidableAction(
+                              backgroundColor: const Color(RootStyle.bgColor),
+                              foregroundColor: Colors.red,
+                              icon: Icons.swipe_down,
+                              label: 'Vender',
+                              onPressed: (BuildContext context) {
+                                _modalTradeInAsset(
+                                    context, widget.portifolio.assets[item]);
+                              },
+                            )
+                          ],
+                        ),
+                        child: ListCardPortifolio(
+                          assets: widget.portifolio.assets[item],
+                        ),
+                      );
+                    },
+                  )
+                : const Center(
+                    child: Text('Sem resultados encontrados.'),
+                  ),
+          ),
         ],
       ),
     );
