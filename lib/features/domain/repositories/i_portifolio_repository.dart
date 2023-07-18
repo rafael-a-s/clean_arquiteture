bb3c7c4c074a3e27dc171bf4ffd9d8f29b8a7dca
Rafael Aguiar <rafaelaguiar@unitins.br]>
2023-07-17 21:21:55 -0300

    feat: Integrando a nova BaseRepository

--- lib/features/domain/repositories/i_portifolio_repository.dart
+++ lib/features/domain/repositories/i_portifolio_repository.dart
@@ -1,11 +1,13 @@
+import 'package:my_app/core/data/repositories/i_base_repository.dart';
 import 'package:my_app/core/usecase/erros/failures.dart';
+import 'package:my_app/features/data/models/portifolio_model.dart';
 import 'package:my_app/features/domain/entities/assets.dart';
 import 'package:my_app/features/domain/entities/portifolio.dart';
 import 'package:dartz/dartz.dart';
 import 'package:my_app/features/domain/entities/portifolio/portifolio_info.dart';
 
-abstract class IPortifolioRepoitory {
-  Future<Either<Failure, List<Portifolio>>> getAllPortifolios();
+abstract class IPortifolioRepoitory
+    extends IBaseRepository<Portifolio, PortifolioModel, dynamic> {
   Future<Either<Failure, PortifolioInfo>> getInfoPortifolio();
   Future<Either<Failure, Portifolio>> createPortifolio(Portifolio assets);
   Future<Either<Failure, Portifolio>> addAssetPortifolio(
